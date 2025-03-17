201e21fae7ec7c219c85a9a760c2fce084e3b7ad
Dan Sapp <dan.sapp@storable.com>
2025-03-17 17:26:50 -0500

    fix cors

--- api/index.py
+++ api/index.py
@@ -68,7 +68,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
