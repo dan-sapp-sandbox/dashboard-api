e137dca4ca311a6f7111dc9eb4eb420759e4e525
Dan Sapp <dan.sapp@storable.com>
2025-03-17 17:38:53 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,9 +9,9 @@ from datetime import datetime, date, timedelta
 from typing import List
 
 # Load JSON data
-def load_data(file_path: str = "api/scatterplot.json"):
-    with open(file_path, "r") as file:
-        return json.load(file)
+# def load_data(file_path: str = "api/scatterplot.json"):
+#     with open(file_path, "r") as file:
+#         return json.load(file)
     
 # def generate_scatterplot_data(size, start, end):
 #    return [{"x": random.randint(start, end), "y": random.randint(start, end)} for _ in range(size)]
@@ -31,37 +31,37 @@ def load_data(file_path: str = "api/scatterplot.json"):
 #     return data
 
 # Define GraphQL types
-@strawberry.type
-class ScatterPoint:
-    x: int
-    y: int
+# @strawberry.type
+# class ScatterPoint:
+#     x: int
+#     y: int
     
-@strawberry.type
-class AreaChartPoint:
-    date: str
-    value: str
+# @strawberry.type
+# class AreaChartPoint:
+#     date: str
+#     value: str
 
 
 
-@strawberry.type
-class Query:
-    @strawberry.field
-    def scatterplot_data(self) -> List[ScatterPoint]:
-        data = load_data("api/scatterplot.json")
-        return [ScatterPoint(**point) for point in data["data"]]
+# @strawberry.type
+# class Query:
+#     @strawberry.field
+#     def scatterplot_data(self) -> List[ScatterPoint]:
+#         data = load_data("api/scatterplot.json")
+#         return [ScatterPoint(**point) for point in data["data"]]
 
-    @strawberry.field
-    def areachart_data(self) -> List[AreaChartPoint]:
-        data = load_data("api/ice_cream_sales.json")
-        return [
-            AreaChartPoint(
-                date=point["date"],
-                value=point["value"]
-            ) 
-            for point in data
-        ]
+#     @strawberry.field
+#     def areachart_data(self) -> List[AreaChartPoint]:
+#         data = load_data("api/ice_cream_sales.json")
+#         return [
+#             AreaChartPoint(
+#                 date=point["date"],
+#                 value=point["value"]
+#             ) 
+#             for point in data
+#         ]
 
-schema = strawberry.Schema(query=Query)
+# schema = strawberry.Schema(query=Query)
 
 app = FastAPI()
 
@@ -74,8 +74,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-graphql_app = GraphQLRouter(schema)
-app.include_router(graphql_app, prefix="/graphql")
+# graphql_app = GraphQLRouter(schema)
+# app.include_router(graphql_app, prefix="/graphql")
 
 @app.get("/")
 def read_root():
